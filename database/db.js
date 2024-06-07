36854cdfda08149de4bc7501eb4ec3c59ff7796d
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-07 12:30:43 +0100

    added schema.sql file and read it with db.js

--- database/db.js
+++ database/db.js
@@ -1,6 +1,16 @@
 require('dotenv').config();
-
+const { readFileSync } = require('node:fs');
+const { join } = require('node:path');
 const Database = require('better-sqlite3');
 
 const db = new Database(process.env.DB_FILE);
-console.log(db);
+
+const schemaPath = join('database', 'schema-sql');
+const schema = readFileSync(schemaPath, 'utf-8');
+db.exec(schema);
+
+const select_date = db.prepare('SELECT DATE()');
+const dateResult = select_date.get();
+console.log(dateResult);
+
+const select_table = db.prepare('SELECT name FROM sqlite_schema');
