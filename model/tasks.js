a3ddfcbc9c73b5276d202963ac90ef9946de94ec
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-07 15:06:34 +0100

    consolidating tasks into one folder

---
+++ model/tasks.js
@@ -0,0 +1,46 @@
+const db = require('../database/db.js');
+
+// Create Task function
+
+const insert_task = db.prepare(/* sql */ `
+	INSERT INTO tasks (content, complete)
+    VALUES ($content, $complete)
+    RETURNING id, content, created_at, complete
+`);
+
+function createTask(task) {
+	return insert_task.get(task);
+}
+
+// List Tasks function
+
+const select_tasks = db.prepare(/* sql */ `
+    SELECT
+        id,
+        content, 
+        TIME(created_at) AS created_at, 
+        complete FROM tasks
+`);
+
+function listTasks() {
+	return select_tasks.all();
+}
+
+console.log(listTasks());
+
+// Delete Tasks function
+
+const delete_task = db.prepare(/* sql */ `
+    DELETE FROM tasks WHERE id = ?
+`);
+
+function removeTask(id) {
+	delete_task.run(id);
+}
+
+removeTask(1);
+
+module.exports = { createTask, listTasks, removeTask };
+
+const result = createTask({ content: 'Make lunch', complete: 0 });
+console.log(result);
---
+++ test/tasks.test.js
@@ -0,0 +1,4 @@
+const test = require('node:test');
+const assert = require('node:assert');
+const model = require('../model/tasks.js');
+const db = require('../database/db.js');
